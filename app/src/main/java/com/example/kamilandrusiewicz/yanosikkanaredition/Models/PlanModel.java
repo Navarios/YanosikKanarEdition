7914d9c181e42d0152fc9f2ff8f40c6fcd864120
Kamil Andrusiewicz <navarios21@gmail.com>
2018-04-23 21:16:31 +0200

    GPS+JSON

---
+++ app/src/main/java/com/example/kamilandrusiewicz/yanosikkanaredition/Models/PlanModel.java
@@ -0,0 +1,76 @@
+package com.example.kamilandrusiewicz.yanosikkanaredition.Models;
+
+public class PlanModel {
+    private String linia;
+    private String trasa;
+    private String z;
+    private String d;
+    private String lat;
+    private String lon;
+    private String punktualnosc1;
+    private String punktualnosc2;
+
+    public String getLinia() {
+        return linia;
+    }
+
+    public void setLinia(String linia) {
+        this.linia = linia;
+    }
+
+    public String getTrasa() {
+        return trasa;
+    }
+
+    public void setTrasa(String trasa) {
+        this.trasa = trasa;
+    }
+
+    public String getZ() {
+        return z;
+    }
+
+    public void setZ(String z) {
+        this.z = z;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getPunktualnosc1() {
+        return punktualnosc1;
+    }
+
+    public void setPunktualnosc1(String punktualnosc1) {
+        this.punktualnosc1 = punktualnosc1;
+    }
+
+    public String getPunktualnosc2() {
+        return punktualnosc2;
+    }
+
+    public void setPunktualnosc2(String punktualnosc2) {
+        this.punktualnosc2 = punktualnosc2;
+    }
+}
